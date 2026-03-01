375cc37a0ca8abf899e83abde3b0fd13fad0ddaa
xLima12 <felipelima1910@gmail.com>
2026-02-28 23:09:34 -0300

    chore(config): adjust core configuration and update application.properties
    - Updated core module configurations
    - Refactored environment properties
    - Adjusted application.properties for proper environment setup

--- backend/src/main/java/dev/flima/backend/config/WebConfig.java
+++ backend/src/main/java/dev/flima/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins("https://autoflex-test-production.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
